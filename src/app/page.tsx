2e11019b92bb2d905a7675a497af74fe218686c2
sumaiya suchi <sumaiyamoina@gmail.com>
2026-02-16 02:27:02 +0600

    adding developer two sifat ullah

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
      <h1>Hello next.js</h1>
+     <p>adding developer2 Sifat Ullah</p>
     </div>
   );
 }
